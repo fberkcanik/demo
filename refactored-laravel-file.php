7595d8b4e0e6a51eb164874d62e852bc40478156
Furkan Berk Canik <52695067+fberkcanik@users.noreply.github.com>
2024-04-16 22:01:12 +0300

    Create refactored-laravel-file.php

---
+++ refactored-laravel-file.php
@@ -0,0 +1,30 @@
+use Illuminate\Http\Request;
+
+public function createPost(Request $request)
+{
+    $title = $request->input('title');
+    $author_id = $request->input('author_id');
+    $content = $request->input('content')
+    $categoryId = $request->input("categoryId");
+
+    $existingPosts = BlogPost::all();
+    foreach($existingPosts as $existing_post){
+      // if we add author id for check, it will allow only 1 post entry for each author.
+         if($existing_post->title == $title){
+            echo "Title already exists";
+            exit;
+         }
+    }
+    $post = new BlogPost;
+    $post->title = $title;
+    $post->content = $content;
+    $post->status = "published";
+    $post->category_id =>
+    $post->save();
+
+   
+
+    // must be added by Model. 
+
+    return "Post created successfully!";
+}
